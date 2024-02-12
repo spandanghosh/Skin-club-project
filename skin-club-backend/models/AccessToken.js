da9f39e69787096005a6162ead555aa43e0d95bc
spandanghosh <72367806+spandanghosh@users.noreply.github.com>
2024-02-12 23:59:00 +0530

    Delete skin-club-backend directory

--- skin-club-backend/models/AccessToken.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const accessSchema = new mongoose.Schema({
-    accessToken: { type: String, required: true },
-    expiration: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('AccessToken', accessSchema)
--- skin-club-backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true },
-    phoneNumber: { type: String, required: true, unique : true },
-    otp: { type: String },
-});
-
-module.exports = mongoose.model('User', userSchema);
