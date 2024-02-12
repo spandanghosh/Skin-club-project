8a205ba2bba00738765b74f0b9960c3123a4138b
spandanghosh <72367806+spandanghosh@users.noreply.github.com>
2024-02-12 23:59:10 +0530

    Delete skin-club directory

--- skin-club/src/env.example.js
+++
@@ -1,2 +0,0 @@
-export const GOOGLE_MAP_API_KEY= ''
-export const BASE_URL= ''
--- skin-club/src/env.js
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_MAP_API_KEY= 'AIzaSyC9jNoCkJyt1Pk6CWpQUDG8TFAkhHo6uT0'
-export const BASE_URL  = 'https://api.skinclubstudios.xyz';
-export const SQUAREUP_APP_ID = 'sq0idp-mcyDs5_ucVHNqliEqTUTVA';
-export const SQUAREUP_LOCATION_ID = 'L5G4NDK8BTJDE';
